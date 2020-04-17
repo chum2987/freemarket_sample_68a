65fb816d3d4a34a4b9d3a2423df0408d13e8690a
seijiDB <seiji_d_mylife.0303@icloud.com>
2020-04-17 23:52:28 +0900

    little_fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :show]
-  resources :users, only [:show]
+  resources :users, only: [:show]
 end
