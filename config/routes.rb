4d6a486aef1adac311dac96ac993a922e23ff2d7
yuta-666 <ilver.silver666@gmail.com>
2020-04-28 12:21:01 +0900

    ルーティングの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :show]
   resources :users, only: [:show] do
