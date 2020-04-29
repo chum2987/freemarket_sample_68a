f4da6872c34d6ed17fa53538179132dea671afde
seijiDB <seiji_d_mylife.0303@icloud.com>
2020-04-29 21:22:25 +0900

    ルーティング直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "items#new"
+  root to: "items#index"
   resources :items, only: [:index, :new, :show]
   resources :users, only: [:show]
 end
