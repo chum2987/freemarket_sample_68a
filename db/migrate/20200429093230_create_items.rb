d93a689445670d295cef893fc7f423ec7ee8865a
tech-yutaro <tanabeyutaro89@gmail.com>
2020-05-01 23:02:32 +0900

    マイグレーションファイルの修正

--- db/migrate/20200429093230_create_items.rb
+++ db/migrate/20200429093230_create_items.rb
@@ -4,7 +4,7 @@ class CreateItems < ActiveRecord::Migration[5.0]
       t.string :name, null: false
       t.integer :price, null: false
       t.text :description, null: false
-      t.references :category_id, null: false
+      t.references :category, null: false
       t.string :size
       t.string :brand
       t.string :condition, null: false
