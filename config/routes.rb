2bb033003e35a5ee691535aae40b17848a757f57
seijiDB <seiji_d_mylife.0303@icloud.com>
2020-05-02 14:54:15 +0900

    conflict_fix2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :new, :show]
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :new] do
     member do
       get "signout"
       get "card"
