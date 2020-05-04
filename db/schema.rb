05e33ec6ffb2bfa2844ebd88a3be62c7437f2eb9
chum2987 <chum2987@gmail.com>
2020-05-04 20:26:23 +0900

    カテゴリー機能seed.rbにてデータの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200429093230) do
+ActiveRecord::Schema.define(version: 20200503113840) do
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.string   "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
+    t.index ["name"], name: "index_categories_on_name", using: :btree
+  end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                          null: false
