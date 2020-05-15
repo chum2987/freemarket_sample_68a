f28df9a8b0506191269bbb58ddb024f9c0401821
judomasaya <ngudct.hayamasa@gmail.com>
2020-05-16 00:49:28 +0900

    不要なコメントアウトの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
-  # belongs_to :buyer, class_name: "User", foreign_key: "buyer_id"
   has_many :item_images
-  # has_one :category
   belongs_to :category
   accepts_nested_attributes_for :item_images, allow_destroy: true
 
