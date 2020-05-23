18d009e92a9fd0ed18c4526200638ddf4560f526
ながのまい <chum2987@gmail.com>
2020-05-23 09:34:19 +0900

    passwordのバリデーション解除

--- db/migrate/20200504122427_create_addresses.rb
+++ db/migrate/20200504122427_create_addresses.rb
@@ -1,6 +1,10 @@
 class CreateAddresses < ActiveRecord::Migration[5.0]
   def change
     create_table :addresses do |t|
+      t.string :family_name, null: false
+      t.string :first_name, null: false
+      t.string :family_name_kana, null: false
+      t.string :first_name_kana, null: false
       t.integer :postal_code, null: false
       t.string :prefecture_name, null: false
       t.string :city, null: false
