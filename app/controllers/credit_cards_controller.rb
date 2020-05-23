7a6ac97403fe0398b38c7a87389c91805ea13d67
yuta-666 <ilver.silver666@gmail.com>
2020-05-23 16:24:48 +0900

    sider修正

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -7,7 +7,6 @@ class CreditCardsController < ApplicationController
   def new
   end
 
-  # payjpとcredit_cardsのデータベースを作成
   def create
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
     if params["payjpToken"].blank?
@@ -15,7 +14,7 @@ class CreditCardsController < ApplicationController
     else
       customer = Payjp::Customer.create(card: params["payjpToken"])
 
-      @card = CreditCard.new( user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card )
+      @card = CreditCard.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to credit_cards_path
       else
