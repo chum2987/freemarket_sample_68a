29a26d1e227d76522f3bf3096778f6f7d4d13ad4
yuta-666 <ilver.silver666@gmail.com>
2020-05-26 19:40:51 +0900

    private-method定義

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,14 +1,14 @@
 class CreditCardsController < ApplicationController
   require "payjp"
+  before_action :set_card
 
   def index
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
+    if @card.blank?
       redirect_to new_credit_card_path
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card = customer.cards.retrieve(card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card = customer.cards.retrieve(@card.card_id)
     end
   end
 
@@ -32,14 +32,18 @@ class CreditCardsController < ApplicationController
   end
 
   def delete
-    card = CreditCard.where(user_id: current_user.id).first
-    if card.blank?
+    if @card.blank?
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       customer.delete
-      card.delete
+      @card.delete
     end
       redirect_to new_credit_card_path
+  end
+
+  private
+  def set_card
+    @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
   end
 end
