afe990d5c52d17ef003d1a8718393105fe0eba31
seijiDB <seiji_d_mylife.0303@icloud.com>
2020-05-29 21:24:56 +0900

    sider修正２

--- app/assets/javascripts/items/sales_profit.js
+++ app/assets/javascripts/items/sales_profit.js
@@ -1,6 +1,6 @@
 $(function(){
   $('#price_calc').on('input', function(){  // リアルタイムで表示したいのでinputを使う。入力の度にイベントが発火するようになる。
-    let data = $('#price_calc').val();  // val()でフォームのvalueを取得(数値)
+    var data = $('#price_calc').val();  // val()でフォームのvalueを取得(数値)
     let profit = Math.round(data * 0.9)  // 手数料計算を行う。dataにかけているのが0.9なのは単に引きたい手数料が10%のため。
     let fee = (data - profit)  // 入力した数値から計算結果(profit)を引くとそれが手数料になる。
     $('.right_bar').html(fee)  // 手数料の表示。html()は追加ではなく上書きする。入力値が変わる度に表示も変わるようにする。
