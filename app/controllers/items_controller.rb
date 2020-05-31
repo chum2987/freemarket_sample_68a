1babb42fce86e293a51a36513f4087a94f31adf1
tech-yutaro <tanabeyutaro89@gmail.com>
2020-05-31 21:02:03 +0900

    商品情報の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -66,7 +66,7 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item)
-      .permit(:name, :price, :description, :category_id, :size, :brand, :condition, :shipping_fee, :shipping_method, :shipping_date, :seller_id, item_images_attributes: [:image_url]).merge(seller_id: current_user.id)
+      .permit(:name, :price, :description, :category_id, :size, :brand, :condition, :shipping_fee, :shipping_method, :shipping_date, :seller_id, item_images_attributes: [:image_url, :destroy, :id]).merge(seller_id: current_user.id)
   end
 
 end
