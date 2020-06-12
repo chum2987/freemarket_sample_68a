2537dd0aaa3b3c93230d5f5138728bea47bb0eab
judomasaya <ngudct.hayamasa@gmail.com>
2020-06-12 20:36:36 +0900

    外部キーのテストは不可ということで削除
    メンターさんから教えていただき
    記述を削除しました！

--- spec/models/item_image_spec.rb
+++ spec/models/item_image_spec.rb
@@ -6,11 +6,5 @@ describe ItemImage do
       itemimage.valid?
       expect(itemimage.errors[:image_url]).to include("can't be blank")
     end
-
-    it "画像が商品と紐づかなければ登録できない" do
-      itemimage = build(:item_image, item_id: "")
-      itemimage.valid?
-      expect(itemimage.errors[:item_id]).to include("can't be blank")
-    end
   end
 end
