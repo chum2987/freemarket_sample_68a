7782e8a3fe81f4ba645fa6d383540abb6d893a53
judomasaya <ngudct.hayamasa@gmail.com>
2020-06-12 20:33:18 +0900

    item_idなどの外部キーにはテストが不要
    メンターさんから
    教えていただき削除しました！

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -2,5 +2,4 @@ class ItemImage < ApplicationRecord
   mount_uploader :image_url, ImageUploader
   belongs_to :item, optional: true
   validates :image_url, presence: true
-  validates :item_id, presence: true
 end
